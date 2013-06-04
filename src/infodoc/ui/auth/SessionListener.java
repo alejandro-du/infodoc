c084eb9bed4c9a24da9aede5ba2a8c0eb745de80
jaduarteav <alejandro.d.a@gmail.com>
2013-06-04 11:45:17 -0500

    Fix NullPointerException in SessionListener.

--- src/infodoc/ui/auth/SessionListener.java
+++ src/infodoc/ui/auth/SessionListener.java
@@ -1,17 +1,11 @@
 package infodoc.ui.auth;
 
-import infodoc.core.ui.auth.AuthService;
-
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-import org.slf4j.LoggerFactory;
-
-import enterpriseapp.hibernate.dto.User;
-
 
 public class SessionListener implements HttpSessionListener {
-
+	
 	/**
      * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
      */
@@ -22,9 +16,6 @@ public class SessionListener implements HttpSessionListener {
      * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
      */
     public void sessionDestroyed(HttpSessionEvent event) {
-    	User user = (User) event.getSession().getAttribute("user");
-    	AuthService.getLoggedUserApps().get(user);
-    	LoggerFactory.getLogger(SessionListener.class).debug("Session destroyed (user id = " + user == null ? "null" : user.getId() + ")");
     }
 	
 }
