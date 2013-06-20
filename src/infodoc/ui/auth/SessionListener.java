d9ff1018a04f404a44e234c3de1a76fd2a6055cb
alejandro-du <alejandro.d.a@gmail.com>
2013-06-20 15:32:29 -0500

    Trace session events.

--- src/infodoc/ui/auth/SessionListener.java
+++ src/infodoc/ui/auth/SessionListener.java
@@ -3,20 +3,26 @@ package infodoc.ui.auth;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class SessionListener implements HttpSessionListener {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SessionListener.class);
 	
 	/**
      * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
      */
     public void sessionCreated(HttpSessionEvent event) {
+    	logger.trace("Session created.");
     }
 
 	/**
      * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
      */
     public void sessionDestroyed(HttpSessionEvent event) {
-    	System.out.println("Session destroyed.");
+    	logger.trace("Session destroyed.");
     }
 	
 }
